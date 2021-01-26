59e3e46a016c731efcb86bcbc26d1298e6fbd1a8
takeru.watanabe <takeru.watanabe>
2021-01-26 13:12:02 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>人生の岐路を共有しよう！</h1>
-            {!! link_to_route('signup.get', '登録', [], ['class' => 'btn btn-lg btn-success']) !!}
+    @if(Auth::check())
+        {{Auth::user()->name}}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>人生の岐路を共有しよう！</h1>
+                {!! link_to_route('signup.get', '登録', [], ['class' => 'btn btn-lg btn-success']) !!}
+            </div>
         </div>
-    </div>
+    @endif
 @endsection
